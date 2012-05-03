b6324ca4ecc41ee3f572bfad505774f68d77eb8d
Rick ter Heide <Rick ter Heide@10.28.34.219>
2012-05-03 16:03:54 +0200

    Added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,9 @@
+<?php
+$language = array (
+  'admin:administer_utilities:deadfile_detector' => 'Deadfile detector',
+  'deadfile_detector:nothing_found' => 'Geen dode bestanden gevonden',
+  'deadfile_detector:dead_files' => 'Dode bestanden',
+  'deadfile_detector:reason:no_file' => 'Fysiek bestand niet gevonden',
+  'deadfile_detector:reason:no_owner' => 'OwnerEntity niet gevonden',
+);
+add_translation("nl", $language);
